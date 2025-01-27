ebab20b2b54eb05bd31749afdca9e76ab44eb4b8
smugii23 <smugii0203@gmail.com>
2025-01-27 14:20:49 -0800

    Created Asteroids game

---
+++ main.py
@@ -0,0 +1,18 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
