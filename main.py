b0a5057c609e025a8e4a965b1b99f42e4d4eab31
smugii23 <smugii0203@gmail.com>
2025-01-27 15:54:39 -0800

    Finished player model, started asteroid model

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for updatables in updatable:
+            updatables.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for drawables in drawable:
+            drawables.draw(screen)
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000
 
 
