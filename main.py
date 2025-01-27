97cf7eb6a1145af656a7a4206a442bb2109a60cd
smugii23 <smugii0203@gmail.com>
2025-01-27 15:09:03 -0800

    Created screen with triangle player model

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
